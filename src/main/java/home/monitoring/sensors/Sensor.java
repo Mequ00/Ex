cc6e2efb2a08a9eca8d32033f9056f5b1443f6fb
Mequ00 <kristin.anastasiya@mail.ru>
2024-06-27 13:52:40 +0300

    Создание системы

--- src/main/java/home/monitoring/sensors/Sensor.java
+++ src/main/java/home/monitoring/sensors/Sensor.java
@@ -13,8 +13,9 @@ public abstract class Sensor<T> {
     //
     private boolean isThresholdExceeded;
     private boolean isDeviceOff;
+
     private double anomalyProbability = 0.05; // начальная вероятность 5%
-    private double disableProbability = 0.05; // 5% вероятность отключения
+    private double disableProbability = 0.05; // вероятность отключения 5%
 
     private LocalDateTime lastUpdateTime;
     private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
@@ -34,22 +35,20 @@ public abstract class Sensor<T> {
         this.anomalyProbability += increment;
     }
 
-    public abstract T generateAnomalyValue();
 
+    public abstract boolean checkThreshold();
+    public abstract T generateAnomalyValue();
     public abstract T generateNormalValue(Random random);
 
     public void generateData() {
         if (isDeviceOff()) {
             return;
         }
-
         if (shouldDisableSensor()) {
             setDeviceOff(true);
             return;
         }
-
         Random random = new Random();
-
         if (shouldGenerateAnomaly() && !isThresholdExceeded) {
             // Генерация аномального значения
             T anomalyValue = generateAnomalyValue();
@@ -87,7 +86,7 @@ public abstract class Sensor<T> {
         return type + " (Текущее значение: " + (isDeviceOff ? "Отключен" : currentValue.toString()) + ")";
     }
 
-    public abstract boolean checkThreshold();
+
 
     public abstract String outputStatus();
 
--- src/main/java/home/monitoring/systems/HomeEngineeringSystem.java
+++ src/main/java/home/monitoring/systems/HomeEngineeringSystem.java
@@ -7,7 +7,6 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -20,18 +19,16 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class HomeEngineeringSystem {
     private String name;
     private List<Sensor<?>> sensors;
-    private BufferedWriter writer;
 
     private boolean thresholdExceededDetected;
 
     private boolean isAnyAnomalyDetected;
 
-    Deque<String> logEntries = new LinkedList<>();
+    private Deque<String> logEntries = new LinkedList<>();
 
 
     Path logsDir = Paths.get("src/main/resources/logs");
@@ -75,15 +72,12 @@ public class HomeEngineeringSystem {
         }
     }
 
-    public void writeToTempLogFile(String message) {
-
+    public void writeToTempLog(String message) {
             // Добавляем новую запись и ограничиваем размер до MAX_LOG_ENTRIES
             logEntries.addLast(message);
             if (logEntries.size() > MAX_LOG_ENTRIES) {
                 logEntries.removeFirst();
             }
-
-
     }
 
     public void writeToLogFile(int countMessages) {
